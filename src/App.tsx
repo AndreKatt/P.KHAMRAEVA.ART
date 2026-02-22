dab6cd81c80a0d202b62f6b68597dbc888bd2d84
AndreKatt <andreevaekatt@gmail.com>
2026-02-22 18:12:21 +0300

    remove huge icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-// import {Header} from "./Components/Header/Header.tsx";
-// import {Info} from "./Components/Info/Info.tsx";
-// import {EdenIllusion} from "./Modules/EdenIllusion/EdenIllusion.tsx";
-// import {Metamorphosis} from "./Modules/Metamorphosis/Metamorphosis.tsx";
+import {Header} from "./Components/Header/Header.tsx";
+import {Info} from "./Components/Info/Info.tsx";
+import {EdenIllusion} from "./Modules/EdenIllusion/EdenIllusion.tsx";
+import {Metamorphosis} from "./Modules/Metamorphosis/Metamorphosis.tsx";
 
 function App() {
   return (
     <div>
-      TEST
-      {/* <Header />
+      <Header />
       <Info />
       <Metamorphosis />
-      <EdenIllusion /> */}
+      <EdenIllusion />
     </div>
   )
 }
