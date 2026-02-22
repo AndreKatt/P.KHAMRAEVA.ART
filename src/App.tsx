a2e2d4daeddece5c6901e112aea4e8d6084f9984
AndreKatt <andreevaekatt@gmail.com>
2026-02-22 17:52:37 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import {Header} from "./Components/Header/Header.tsx";
-import {Info} from "./Components/Info/Info.tsx";
-import {EdenIllusion} from "./Modules/EdenIllusion/EdenIllusion.tsx";
-import {Metamorphosis} from "./Modules/Metamorphosis/Metamorphosis.tsx";
+// import {Header} from "./Components/Header/Header.tsx";
+// import {Info} from "./Components/Info/Info.tsx";
+// import {EdenIllusion} from "./Modules/EdenIllusion/EdenIllusion.tsx";
+// import {Metamorphosis} from "./Modules/Metamorphosis/Metamorphosis.tsx";
 
 function App() {
   return (
     <div>
-      <Header />
+      TEST
+      {/* <Header />
       <Info />
       <Metamorphosis />
-      <EdenIllusion />
+      <EdenIllusion /> */}
     </div>
   )
 }
