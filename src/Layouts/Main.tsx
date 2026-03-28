659b115261537c74626a9f1e918b41374be935fb
AndreKatt <andreevaekatt@gmail.com>
2026-03-28 16:21:43 +0300

    add reset scroll on navigation

--- src/Layouts/Main.tsx
+++ src/Layouts/Main.tsx
@@ -1,8 +1,15 @@
 import {Container} from '../Components/Container/Container'
-import {Outlet} from 'react-router'
+import {Outlet, useLocation} from 'react-router'
 import {Header} from '../Modules/Header/Header'
+import { useEffect } from 'react'
 
 export const MainLayout = () => {
+  const {pathname} = useLocation()
+
+  useEffect(() => {
+    document.body.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <>
       <Header />
